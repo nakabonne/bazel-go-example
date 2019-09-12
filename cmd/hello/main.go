d0df798b3f658ed1b417d59efd4755efe87ac556
Ryo Nakao <nakabonne@gmail.com>
2019-09-12 16:45:48 +0900

    Add foo

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("vim-go")
 	_, _ = bar()
+	_, _ = foo()
 }
 
 func bar() ([]int, error) {
+	nums := []int{1, 2, 3}
+	return nums[1:len(nums)], nil
+}
+
+func foo() ([]int, error) {
 	nums := []int{1, 2, 3}
 	return nums[1:len(nums)], nil
 }
